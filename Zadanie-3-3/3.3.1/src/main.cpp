84b8070cd1911b297005f61479c4919729aaecdc
„Maria <maria.gromek@student.wat.edu.pl>
2021-01-11 09:26:05 +0100

    „Wykonano zadanie 3.3.1 – tekst statyczny”.

---
+++ Zadanie-3-3/3.3.1/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+//Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7,
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  lcd.begin(16,2);  // ustawienie liczby kolumn i wierszy
+  lcd.print("Gromek"); // Komunikat powitalny
+}
+
+void loop() {
+  lcd.setCursor(0, 1); // Przesunięcie kursora do początku drugiego wiersza
+  lcd.print(millis() / 1000); / Wyświetlenie liczby sekund od uruchomienia
+  delay(500);
+}
